5f86e41ff26edd543b6cceddc754285dc4283059
mahdirafie <mahdi47rafie@gmail.com>
2024-01-10 12:57:41 +0330

    ready to go

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parvaz_event/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,116 +11,56 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const TextStyle defaultTextStyle = TextStyle(fontFamily: 'IranYekan', color: Colors.white);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        textTheme: TextTheme(
+          displayLarge: defaultTextStyle.copyWith(
+              fontSize: 96, fontWeight: FontWeight.w300),
+          displayMedium: defaultTextStyle.copyWith(
+              fontSize: 60, fontWeight: FontWeight.w400),
+          displaySmall: defaultTextStyle.copyWith(
+              fontSize: 48, fontWeight: FontWeight.w400),
+          headlineMedium: defaultTextStyle.copyWith(
+              fontSize: 34, fontWeight: FontWeight.w400),
+          headlineSmall: defaultTextStyle.copyWith(
+              fontSize: 24, fontWeight: FontWeight.w400),
+          titleLarge: defaultTextStyle.copyWith(
+              fontSize: 20, fontWeight: FontWeight.w600),
+          titleMedium: defaultTextStyle.copyWith(
+              fontSize: 18, fontWeight: FontWeight.w500),
+          bodyLarge: defaultTextStyle.copyWith(
+              fontSize: 16, fontWeight: FontWeight.w400),
+          bodyMedium: defaultTextStyle.copyWith(
+              fontSize: 14, fontWeight: FontWeight.w400),
+          bodySmall: defaultTextStyle.copyWith(
+              fontSize: 12, fontWeight: FontWeight.w400, color: Colors.white38),
+          labelLarge: defaultTextStyle.copyWith(
+              fontSize: 14, fontWeight: FontWeight.w500),
         ),
+        colorScheme: const ColorScheme.dark(
+          primary: LightTheme.primaryColor,
+          onPrimary: LightTheme.onPrimaryColor,
+          primaryContainer: LightTheme.primaryVariantColor,
+          onPrimaryContainer: LightTheme.onPrimaryVariantColor,
+          surface: LightTheme.surfaceColor,
+          onSurface: LightTheme.onSurfaceColor,
+          background: LightTheme.backgroundColor,
+          onBackground: LightTheme.onBackgroundColor
+        ),
+        useMaterial3: true,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12)
+          )
+        ),
+        elevatedButtonTheme: const ElevatedButtonThemeData(
+          style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(LightTheme.primaryColor), foregroundColor: MaterialStatePropertyAll(LightTheme.onPrimaryColor), )
+        ),
+        dividerTheme: const DividerThemeData(color: Colors.grey, indent: 12, endIndent: 12, space: 0)
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // home: const Home()
     );
   }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LightTheme {
+  static const Color primaryColor = Color(0xff0038F5);
+  static const Color onPrimaryColor = Colors.white;
+  static const Color primaryVariantColor = Colors.transparent;
+  static const Color onPrimaryVariantColor = Colors.black;
+  static const Color surfaceColor = Colors.white;
+  static const Color onSurfaceColor = Colors.black;
+  static const Color backgroundColor = Color(0xffF8F8F8);
+  static const Color onBackgroundColor = Colors.black;
+}
